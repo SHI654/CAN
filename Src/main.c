b2faea48bd754e8f309251c7bd64ff7f17d999db
Yujie <1633838341@qq.com>
2024-08-30 16:43:30 +0800

    led

--- Src/main.c
+++ Src/main.c
@@ -98,7 +98,8 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+      HAL_GPIO_TogglePin(LED_GPIO_Port,LED_Pin);
+      HAL_Delay(1000);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
